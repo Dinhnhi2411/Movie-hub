52f8a31a87fa0aa2cd0cc7ae5ed88760c2d68bd6
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-03-24 14:01:01 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
+import ThemeProvider from "./contexts/ThemeProvider";
+import { AuthProvider } from "./contexts/AuthContext";
+import "./style.css";
+
+function App() {
+  return (
+   
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
