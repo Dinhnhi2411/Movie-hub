6342c6ec265780defc3405808d5e1087e7dfff1e
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-03-24 14:00:45 +0700

    Create Carousel.js

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./Carousel.css"
+
+const handleDragStart = (e) => e.preventDefault();
+
+const Carousel = ({ id, media_type }) => {
+  const [credits, setCredits] = useState([]);
+
+  const items = credits?.map((c) => (
+    <div className="carouselItem">
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${c.profile_path}`}
+        alt={c?.name}
+        onDragStart={handleDragStart}
+        className="carouselItem__img"
+      />
+      <b className="carouselItem__txt">{c?.name}</b>
+    </div>
+  ));
+
+  const responsive = {
+    0: {
+      items: 3,
+    },
+    512: {
+      items: 5,
+    },
+    1024: {
+      items: 7,
+    },
+  };
+
+  const fetchCredits = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=f3d8e4feb56f8aa83c7956ede155fe74&language=en-US`
+    );
+    setCredits(data.cast);
+  };
+
+  useEffect(() => {
+    fetchCredits();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <AliceCarousel
+      mouseTracking
+      infinite
+      disableDotsControls
+      disableButtonsControls
+      responsive={responsive}
+      items={items}
+      autoPlay
+    />
+  );
+};
+
+export default Carousel
